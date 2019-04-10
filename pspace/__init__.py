02684520340bf3f572fbc5e079149ff3004f7414
Matthew Clapp <itsayellow+dev@gmail.com>
2019-04-10 14:02:37 -0700

    Add job_started to imports, rearrange.

--- pspace/__init__.py
+++ pspace/__init__.py
@@ -1,9 +1,7 @@
 from .pspace import (
+        job_started, job_not_started, job_not_done,
         jobs_create, jobs_list,
-        job_done, job_not_started,
-        get_log_lines,
-        get_artifacts, save_log, follow_log,
-        get_config,
-        get_job_info,
+        get_log_lines, get_artifacts, save_log, follow_log,
+        get_job_info, get_config,
         save_last_info, get_last_info,
         )
