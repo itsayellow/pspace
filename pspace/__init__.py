1a573bfd1f1eefce310c1cdeb0ebe33108d2b601
Matthew Clapp <itsayellow+dev@gmail.com>
2019-04-14 23:26:04 -0700

    Prune exported functions.

--- pspace/__init__.py
+++ pspace/__init__.py
@@ -1,10 +1,10 @@
 from .pspace import (
-        job_started, job_not_started, job_done,
         jobs_create, jobs_list,
-        get_log_lines, get_artifacts, save_log, follow_log,
-        get_job_info, get_yaml_config,
+        get_artifacts, save_log,
+        get_job_info,
         stop_job,
-        save_last_info, get_last_info,
-        get_cmd_config, print_job_status, print_last_log_lines, print_error,
-        CMD_ARG_DEFAULTS,
+        save_last_info,
+        get_cmd_config, print_job_status,
+        print_last_log_lines, print_error,
+        save_new_yaml_config,
         )
