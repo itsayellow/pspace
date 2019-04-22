9ea2fd12e777a392f2127d653327cb45a2986431
Matthew Clapp <itsayellow+dev@gmail.com>
2019-04-22 14:16:53 -0700

    Add print_create_options.

--- pspace/__init__.py
+++ pspace/__init__.py
@@ -5,6 +5,6 @@ from .pspace import (
         stop_job,
         save_last_info,
         get_cmd_config, print_job_status,
-        print_last_log_lines, print_error,
+        print_last_log_lines, print_error, print_create_options,
         save_new_yaml_config,
         )
