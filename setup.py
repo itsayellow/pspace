ea43729bb0791a60aa27b7ed0834e93b513fa76a
Matthew Clapp <itsayellow+dev@gmail.com>
2019-11-07 19:39:21 -0800

    Add url, reformat via black, remove python 2.

--- setup.py
+++ setup.py
@@ -6,30 +6,22 @@ from setuptools import setup
 here = os.path.abspath(os.path.dirname(__file__))
 
 setup(
-        name='pspace',
-        version='0.1',
-        description='Support for running jobs on Paperspace cloud computing',
-        author='Matthew Clapp',
-        author_email='itsayellow+dev@gmail.com',
-        license='MIT',
-        classifiers=[
-            'Development Status :: 3 - Alpha',
-            'Intended Audience :: Developers',
-            'License :: OSI Approved :: MIT License',
-            'Programming Language :: Python :: 3'
-            'Programming Language :: Python :: 2'
-            ],
-        keywords='cloud paperspace',
-        packages=['pspace'],
-        install_requires=[
-            'paperspace<0.2.0',
-            'pyyaml',
-            ],
-        entry_points={
-            'console_scripts':[
-                'pspace=pspace.cli:cli'
-                ]
-            },
-        python_requires='>=3',
-        )
-
+    name="pspace",
+    version="0.1",
+    description="Support for running jobs on Paperspace cloud computing",
+    author="Matthew Clapp",
+    author_email="itsayellow+dev@gmail.com",
+    license="MIT",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+    ],
+    keywords="cloud paperspace",
+    url="https://github.com/itsayellow/pspace",
+    packages=["pspace"],
+    install_requires=["paperspace<0.2.0", "pyyaml"],
+    entry_points={"console_scripts": ["pspace=pspace.cli:cli"]},
+    python_requires=">=3",
+)
